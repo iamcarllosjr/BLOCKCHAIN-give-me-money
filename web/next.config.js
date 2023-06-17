f579e2b94bc8f4f724badb7fcd35592354c1e11a
Carlos <iamcarllosjr@gmail.com>
2023-06-16 21:42:43 -0300

    alterando next.config

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    webpack: (config) => {
-      config.externals.push({
-        'utf-8-validate': 'commonjs utf-8-validate',
-        'bufferutil': 'commonjs bufferutil',
-      })
-      return config
-    },
-  }
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    })
+    return config
+  },
+}
--- web/src/app/components/Navbar.tsx
+++ web/src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import Logo from "../../../public/logo.png"
+import Logo from "../../../public/logo.png";
 
 const Navbar = () => {
     const [walletAddress, setWalletAddress] = useState("");
